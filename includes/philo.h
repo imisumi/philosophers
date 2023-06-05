678a215ab46db7c5835e6917d3f9a6a2c1556685
imisumi <124260839+imisumi@users.noreply.github.com>
2023-06-05 16:04:17 +0200

    push

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: imisumi <imisumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:15:55 by ichiro            #+#    #+#             */
-/*   Updated: 2023/06/05 14:48:31 by imisumi          ###   ########.fr       */
+/*   Updated: 2023/06/05 15:08:41 by imisumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,22 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <pthread.h>
+# include <stdbool.h>
 
+typedef struct s_philo
+{
+	/* data */
+}	t_philo;
+
+typedef struct s_data
+{
+	int				philo_count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				times_to_eat;
+	pthread_mutex_t	*forks;
+}	t_data;
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,25 @@
 /*   By: imisumi <imisumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 11:54:34 by imisumi           #+#    #+#             */
-/*   Updated: 2023/06/05 14:50:06 by imisumi          ###   ########.fr       */
+/*   Updated: 2023/06/05 16:03:34 by imisumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-
-int	main(int argc, char *argv[], char *envp[])
+bool	setup(t_data *data, char **argv)
 {
+	
+	return true;
+}
+
+int	main(int argc, char *argv[])
+{
+	t_data	data;
+
+	if (argc != 5 && argc != 6)
+		return (1);
+	if (setup(&data, argv) == false)
+		return (2);
 	return 1;
 }
